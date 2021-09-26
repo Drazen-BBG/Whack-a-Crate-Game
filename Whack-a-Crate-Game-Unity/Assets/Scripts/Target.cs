677320436511c075b3475ef6c3a0e03c8babb970
Drazen Drinic <drazendrinic@hotmail.com>
2021-09-26 21:36:02 +0200

    Toss objects randomly in the air

--- Whack-a-Crate-Game-Unity/Assets/Scripts/Target.cs
+++ Whack-a-Crate-Game-Unity/Assets/Scripts/Target.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody target_RB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        target_RB = GetComponent<Rigidbody>();
+
+        target_RB.AddForce(Vector3.up * Random.Range(12, 18), ForceMode.Impulse);
+        target_RB.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+
+        transform.position = new Vector3(Random.Range(-4, 4), -1, 0);
     }
 
     // Update is called once per frame
